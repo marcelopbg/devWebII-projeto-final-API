32af5920fa8d3c100505c405f69d9f776dc0b267
marcelopbg <mpbguima@gmail.com>
2021-02-15 20:14:00 -0300

    remove unecessary route

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 require('./routes/auth.routes')(app);
-require('./routes/user.routes')(app);
 require('./routes/house.routes')(app);
 
 const PORT = process.env.PORT || 8080;
