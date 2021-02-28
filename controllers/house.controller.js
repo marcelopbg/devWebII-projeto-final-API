26340dc783ad4a528a9ca088862dffdffe413f93
pedr <pedrlz.frn@gmail.com>
2021-02-27 22:14:13 -0300

    Fazendo join da tabela de rent e house

--- controllers/house.controller.js
+++ controllers/house.controller.js
@@ -1,37 +1,40 @@
 const db = require("../models");
 const House = db.house;
+const Rent = db.rent;
 const { Op } = require("sequelize");
 
 const getHouses = (req, res, next) => {
-    if (req.query) {
-        let mappedWhere = {}
-        Object.entries(req.query).map(([key, value]) => {
-            if (Array.isArray(value)) {
-                if (value[0] === "between") {
-                    mappedWhere[key] = { [Op.between]: [value[1], value[2]] }
-                }
-                if (value[0] == "dateRangeBetween") {
-                    mappedWhere.availableForRentStartDate = { [Op.lte]: new Date(value[1]) };
-                    mappedWhere.availableForRentEndDate = { [Op.gte]: new Date(value[2]) }
-                }
-            } else {
-                mappedWhere[key] = value;
-            }
-        })
-        House.findAll({
-            where:
-                mappedWhere
-        }).then(houses => {
-            res.send(houses);
-        })
-            .catch(function (err) {
-                console.log('DB ERROR: ' + err.message);
-                res.status(500);
-                next(err);
-            });
-        return;
-    }
-    House.findAll().then(houses => {
+    // if (req.query) {
+    //     let mappedWhere = {}
+    //     Object.entries(req.query).map(([key, value]) => {
+    //         if (Array.isArray(value)) {
+    //             if (value[0] === "between") {
+    //                 mappedWhere[key] = { [Op.between]: [value[1], value[2]] }
+    //             }
+    //             if (value[0] == "dateRangeBetween") {
+    //                 mappedWhere.availableForRentStartDate = { [Op.lte]: new Date(value[1]) };
+    //                 mappedWhere.availableForRentEndDate = { [Op.gte]: new Date(value[2]) }
+    //             }
+    //         } else {
+    //             mappedWhere[key] = value;
+    //         }
+    //     })
+    //     House.findAll({
+    //         where:
+    //             mappedWhere
+    //     }).then(houses => {
+    //         res.send(houses);
+    //     })
+    //         .catch(function (err) {
+    //             console.log('DB ERROR: ' + err.message);
+    //             res.status(500);
+    //             next(err);
+    //         });
+    //     return;
+    // }
+    House.findAll(
+        { include: Rent }).then(houses => {
+        console.log(houses.rent)
         res.send(houses);
     })
         .catch(function (err) {
--- models/rent.js
+++ models/rent.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      rent.belongsTo(models.house);
     }
   };
   rent.init({
